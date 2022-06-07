255a548d61fd03aab1c58669bd6b561036003137
Vikas Vishwakarma <vikasvishwakarma6787@gmail.com>
2022-06-07 09:16:58 +0530

    [Add] Parameterize constructor with all data members required as arguments

--- src/dated_2022_06_06/Q39_Movie.java
+++ src/dated_2022_06_06/Q39_Movie.java
@@ -1,9 +1,16 @@
 package dated_2022_06_06;
 
 public class Q39_Movie {
-	
+
 	private String title;
 	private String studio;
 	private String rating;
 	
+	public Q39_Movie(String title, String studio, String rating) {
+		super();
+		this.title = title;
+		this.studio = studio;
+		this.rating = rating;
+	}
+
 }
